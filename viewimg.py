7408eeca6e51f2a81b3c45b3a371d51db93cebda
0mgRod <89850217+0mgRod@users.noreply.github.com>
2023-07-08 18:57:18 +0100

    Update viewimg.py
    - Added GZIP HTML support

--- viewimg.py
+++ viewimg.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import tempfile
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from tkinter import Tk, filedialog
+import gzip
 
 
 class HtmlWindow(QMainWindow):
@@ -28,10 +30,20 @@ if __name__ == '__main__':
     root = Tk()
     root.withdraw()
 
-    # Ask for the HTML file to view
-    html_file_path = filedialog.askopenfilename(title="Select HTML File", filetypes=[("HTML Files", "*.html")])
+    # Define the file type filter
+    file_types = [("HTML Files", "*.html"), ("GZ Files", "*.gz")]
+
+    # Ask for the HTML or GZ file to view
+    html_file_path = filedialog.askopenfilename(title="Select HTML or GZ File", filetypes=file_types)
     if not html_file_path:
         sys.exit()
+
+    # Decompress the file if it is a GZ file
+    if html_file_path.endswith(".gz"):
+        uncompressed_path = os.path.join(tempfile.gettempdir(), "output.html")
+        with open(uncompressed_path, 'wb') as output_file, gzip.open(html_file_path, 'rb') as input_file:
+            output_file.write(input_file.read())
+        html_file_path = uncompressed_path
 
     # Open HTML in PyQt5 window
     app = QApplication(sys.argv)
