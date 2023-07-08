3f5fed02eed395f962d9439ef01d3e7b85121243
0mgRod <89850217+0mgRod@users.noreply.github.com>
2023-07-08 18:56:02 +0100

    Update convert.py
    - Added GZIP compression
    - Added image downscale

--- convert.py
+++ convert.py
@@ -3,14 +3,21 @@ import os
 from PIL import Image
 from tkinter import Tk, filedialog
 from tqdm import tqdm
+import gzip
 
 
-def convert_image_to_html(image_path, output_path):
+def convert_image_to_html(image_path, output_path, scale_factor, compress):
     # Open the image
     image = Image.open(image_path)
 
     # Get image dimensions
     width, height = image.size
+
+    # Adjust the dimensions based on the scale factor
+    if scale_factor != 1:
+        width = int(width / scale_factor)
+        height = int(height / scale_factor)
+        image = image.resize((width, height))
 
     # Start building the HTML content
     html_content = '<!DOCTYPE html>\n'
@@ -53,17 +60,39 @@ def convert_image_to_html(image_path, output_path):
     with open(output_path, 'w') as output_file:
         output_file.write(html_content)
 
+    # Compress the output file with gzip if requested
+    if compress:
+        compressed_path = output_path + '.gz'
+        with open(output_path, 'rb') as input_file, gzip.open(compressed_path, 'wb') as output_file:
+            output_file.writelines(input_file)
+        os.remove(output_path)  # Remove the uncompressed file
+        return compressed_path
+    else:
+        return output_path
+
 
 if __name__ == '__main__':
     # Get the absolute paths of the files
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    image_file = filedialog.askopenfilename(title="Select image file", filetypes=[("PNG Image", "*.png"), ("JPEG Image", "*.jpg")])
+    image_file = filedialog.askopenfilename(title="Select image file",
+                                            filetypes=[("PNG Image", "*.png"), ("JPEG Image", "*.jpg")])
     if not os.path.isabs(image_file):
         image_file = os.path.join(current_dir, image_file)
 
     html_file = os.path.join(current_dir, 'output.html')
 
-    # Convert image to HTML
-    convert_image_to_html(image_file, html_file)
+    # Ask for the scale factor
+    scale_factor_str = input("Enter the scale factor (type 1 for same size, 2 for 2x smaller etc): ")
+    if scale_factor_str.strip():
+        scale_factor = float(scale_factor_str)
+    else:
+        scale_factor = 1
 
-    print(f"HTML file generated: {html_file}")
+    # Ask if gzip compression should be applied
+    compress_str = input("Compress with gzip? (y/n): ")
+    compress = compress_str.lower() == 'y' if compress_str.strip() else False
+
+    # Convert image to HTML
+    output_file = convert_image_to_html(image_file, html_file, scale_factor, compress)
+
+    print(f"HTML file generated: {output_file}")
